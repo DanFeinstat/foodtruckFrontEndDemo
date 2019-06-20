c7f8f7fa1b106dd5b9bcd5ea46e4d00d153007a5
DanFein <danfeinstat@gmail.com>
2019-06-19 18:29:22 -0700

    Updates

---
+++ precache-manifest.b2e1e499c30633779aeb24f005e5d5a8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "676285222d27dd4fa109a21a78f9ba3c",
+    "url": "/foodtruckFrontEndDemo/index.html"
+  },
+  {
+    "revision": "f2bb778f2eef9358b257",
+    "url": "/foodtruckFrontEndDemo/static/css/2.a1a5a738.chunk.css"
+  },
+  {
+    "revision": "7602bb56d28d2d57136f",
+    "url": "/foodtruckFrontEndDemo/static/css/main.6f38b8a3.chunk.css"
+  },
+  {
+    "revision": "f2bb778f2eef9358b257",
+    "url": "/foodtruckFrontEndDemo/static/js/2.51a7c1a7.chunk.js"
+  },
+  {
+    "revision": "7602bb56d28d2d57136f",
+    "url": "/foodtruckFrontEndDemo/static/js/main.1da37862.chunk.js"
+  },
+  {
+    "revision": "ed6b3484bdc1031f4923",
+    "url": "/foodtruckFrontEndDemo/static/js/runtime~main.753aafc9.js"
+  },
+  {
+    "revision": "e4ecf5ea49b6a8c9f25a27558511e428",
+    "url": "/foodtruckFrontEndDemo/static/media/HambreLogo.e4ecf5ea.png"
+  },
+  {
+    "revision": "daf07576c986eb5124654a4da3d92bf4",
+    "url": "/foodtruckFrontEndDemo/static/media/foodMapMarker.daf07576.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/foodtruckFrontEndDemo/static/media/logo.5d5d9eef.svg"
+  }
+]);
